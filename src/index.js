2fd975584f54b4f1275309011010250583e51c71
sarahcolpitts <44707123+sarahcolpitts@users.noreply.github.com>
2019-02-05 09:41:05 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// Bootstrap Screen Sizes
-// xs (for phones - screens less than 768px wide)
-// sm (for tablets - screens equal to or greater than 768px wide)
-// md (for small laptops - screens equal to or greater than 992px wide)
-// lg (for laptops and desktops - screens equal to or greater than 1200px wide)
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
