0f149deebe19ed2d3214e63d0238eabb72f16a43
sarahcolpitts <44707123+sarahcolpitts@users.noreply.github.com>
2019-03-07 14:44:16 -0600

    Update Login.js

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+//import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import firebase from '../firebase.js';
 import "./Login.css";
 
@@ -21,7 +21,6 @@ export default class Login extends Component {
       this.setState({
         [event.target.id]: event.target.value
       });
-      //console.log(this.state.username); // 0
     }
   
     handleSubmit = event => {
@@ -30,88 +29,70 @@ export default class Login extends Component {
 
     signUpSubmit = event => {
       firebase.auth().signInWithEmailAndPassword(this.username, this.password).catch(function(error) {
+        
         // Handle Errors here.
         console.log("working");
         var errorCode = error.code;
         var errorMessage = error.message;
-        // ...
       });
     }
 
     render() {
       return (
         <div className="Login">
-          <form onSubmit={this.handleSubmit}>
-            <FormGroup controlId="username" bsSize="large">
-              <ControlLabel>Username</ControlLabel>
-              <FormControl
-                autoFocus
-                type="username"
-                value={this.state.username}
-                onChange={this.handleChange}
-              />
-            </FormGroup>
-            <FormGroup controlId="password" bsSize="large">
-              <ControlLabel>Password</ControlLabel>
-              <FormControl
-                value={this.state.password}
-                onChange={this.handleChange}
-                type="password"
-              />
-            </FormGroup>
-            <Button
-              className="loginButton"
-              bsSize="large"
-              onClick={this.handleChange}
-              //disabled={!this.validateForm()}
-            >
-              Login
-            </Button>
+          <form className="container" onSubmit={this.handleSubmit}>
+            <label className="username">USERNAME</label>
+            <br/>
+            <input type="text" placeholder="enter username" id="username" value={this.state.username} onChange={this.handleChange} autoFocus required />
+            <br/>
+            <label className="password">PASSWORD</label>
+            <br/>
+            <input type="password" placeholder="enter password" id="password" value={this.state.password} onChange={this.handleChange} required />
+            <br/> 
+            <br/> 
+
+            <button type="submit" onClick={this.signUpSubmit}>LOG IN</button>
+            <br/>
+
+            <span className="password-retrieve"><a href="#">Forgot password?</a></span>
           </form>
-
-          <div className="loggedIn">
-            <h3>Welcome User.</h3>
-            <p>You are currently logged in</p>
-            <Button>Logout</Button>
-          </div>
-
-          <div className="signUp">
-              <h3>Don't have an account yet?</h3>
-              <p>Sign up here</p>
-              <FormGroup controlId="username" bsSize="large">
-              <ControlLabel>Username</ControlLabel>
-              <FormControl
-                autoFocus
-                type="username"
-                value={this.state.username}
-                onChange={this.handleChange}
-              />
-            </FormGroup>
-            <FormGroup controlId="password" bsSize="large">
-              <ControlLabel>Password</ControlLabel>
-              <FormControl
-                value={this.state.password}
-                onChange={this.handleChange}
-                type="password"
-              />
-            </FormGroup>
-            <Button
-              className="signUpButton"
-              bsSize="large"
-              onClick={this.signUpSubmit}
-
-              //disabled={!this.validateForm()}
-            >
-              Sign Up
-            </Button>
-          </div>
-
-          <div className="recoverPassword">
-            <p>Click on this to recover password</p>
-          </div>
-
         </div>
 
       );
     }
-  }
+}
+
+/*
+<div className="signUp">
+  <h3>Don't have an account yet?</h3>
+  <p>Sign up here</p>
+  <FormGroup controlId="username" bsSize="large">
+  <ControlLabel>Username</ControlLabel>
+  <FormControl
+    autoFocus
+    type="username"
+    value={this.state.username}
+    onChange={this.handleChange}
+  />
+  </FormGroup>
+  <FormGroup controlId="password" bsSize="large">
+  <ControlLabel>Password</ControlLabel>
+  <FormControl
+    value={this.state.password}
+    onChange={this.handleChange}
+    type="password"
+  />
+  </FormGroup>
+  <Button
+    className="signUpButton"
+    bsSize="large"
+    onClick={this.signUpSubmit}
+    //disabled={!this.validateForm()}
+  >
+    Sign Up
+  </Button>
+</div>
+    <div className="recoverPassword">
+    <p>Click on this to recover password</p>
+    </div>
+*/
